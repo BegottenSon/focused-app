ef9dddceccdf08e05b52dbc5504cb73f68018f70
BegottenSon <danielbrown2d@gmail.com>
2021-11-25 15:25:58 -0500

    debug auth

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ module.exports = {
     FIREBASE_MESSENGING_SENDER: "988699689626",
     FIREBASE_API_ID: "1:988699689626:web:76c687c0ac9717ce361523",
     FIREBASE_MEASUREMENT_ID: "G-HV3YB1HD3R",
-    AUTH0_BASE_URL: '$VERCEL_URL'
+    AUTH0_BASE_URL: 'https://focused-app.vercel.app/'
   },
   images: {
     domains: ['lh3.googleusercontent.com']
