b5ed78bece054c2fd5f44b534a7dc1b852802939
BegottenSon <danielbrown2d@gmail.com>
2021-11-26 06:40:39 -0500

    add auth to next config

--- next.config.js
+++ next.config.js
@@ -1,15 +1,21 @@
 module.exports = {
   reactStrictMode: true,
   env: {
-    FIREBASE_API_KEY: "AIzaSyBNcTh90q99RaOUtF26ebzUUkCO7Vn60pM",
-    FIREBASE_AUTH_DOMAIN: "focused-goals.firebaseapp.com",
-    FIREBASE_PROJECT_ID: "focused-goals",
-    FIREBASE_STORAGE_BUCKET: "focused-goals.appspot.com",
-    FIREBASE_MESSENGING_SENDER: "988699689626",
-    FIREBASE_API_ID: "1:988699689626:web:76c687c0ac9717ce361523",
-    FIREBASE_MEASUREMENT_ID: "G-HV3YB1HD3R",
+    FIREBASE_API_KEY: 'AIzaSyBNcTh90q99RaOUtF26ebzUUkCO7Vn60pM',
+    FIREBASE_AUTH_DOMAIN: 'focused-goals.firebaseapp.com',
+    FIREBASE_PROJECT_ID: 'focused-goals',
+    FIREBASE_STORAGE_BUCKET: 'focused-goals.appspot.com',
+    FIREBASE_MESSENGING_SENDER: '988699689626',
+    FIREBASE_API_ID: '1:988699689626:web:76c687c0ac9717ce361523',
+    FIREBASE_MEASUREMENT_ID: 'G-HV3YB1HD3R',
+    AUTH0_SECRET: 'use [openssl rand -hex 32] to generate a 32 bytes value',
+    AUTH0_BASE_URL: 'https://focused-app.vercel.app',
+    AUTH0_ISSUER_BASE_URL: 'https://dev-tbe1iryp.us.auth0.com',
+    AUTH0_CLIENT_ID: 'Rt0rqh0GP9JXFURjuA4wsXQPIU6fRJ7g',
+    AUTH0_CLIENT_SECRET:
+      '0XhPdQsblFtR_M6s6CQZSrcdxCowOHqM5S4unMy0ijcIvVnGH-mIM4X_WwNNSzYV',
   },
   images: {
-    domains: ['lh3.googleusercontent.com']
-  }
-}
+    domains: ['lh3.googleusercontent.com'],
+  },
+};
